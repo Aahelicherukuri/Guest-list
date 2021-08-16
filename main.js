3dc24d9555332afcc88bde4dfc53b55ce2058545
Aahelicherukuri <88046468+Aahelicherukuri@users.noreply.github.com>
2021-08-16 21:47:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var GuestName = document.getElementById("name1").value;
+// use the push function as -names_of_people.push(Guestname);
+document.getElementById("  ").style.display = "block"
+    // display the name in the id= sort_button
+names_of_people //add the sort funtion
+//. the sort funtion is . sort();
